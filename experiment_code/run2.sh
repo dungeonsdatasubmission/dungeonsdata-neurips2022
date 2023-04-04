031063939891d0d4ecd3dbcc9d4c9bb9e4575b6d
Bartłomiej Cupiał <bartomiej.cupial@student.uj.edu.pl>
2023-04-04 10:07:42 +0200

    dgx script

---
+++ experiment_code/run2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH --job-name=NetHackRL
+#SBATCH --output=logger-%j.txt
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=10
+#SBATCH --mem-per-cpu=2G
+#SBATCH --gres=gpu:1
+#SBATCH --qos=quick  # test (1 GPU, 1 hour), quick (1 GPU, 1 day), normal (2 GPU, 2 days), big (4 GPU, 7 days)
+#SBATCH --partition=dgxmatinf  # dgxa100 (mini-servers), dgxteam (dgx1 for Team-Net), dgxmatinf (dgx2 for WMiI), dgx (dgx1 and dgx2)
+
+#squeue -u ${USER} --Format "JobID:.6 ,Partition:.4 ,Name:.10 ,StateCompact:.2 ,TimeUsed:.11 ,Qos:.7 ,TimeLeft:.11 ,ReasonList:.16 ,Command:.40"
+
+singularity exec --nv -H /home/z1188643/dungeonsdata-neurips2022/experiment_code --env WANDB_API_KEY=d2f9309c1cee36dc7ad726c57e4eba04974d9914 --env WANDBPWD=$PWD -B /shared/results/z1188643/dungeons/nle:/nle -B $TMPDIR:/tmp /shared/results/z1188643/dungeons/dungeons.sif ./train.sh
+
+
+# singularity shell --nv -H /home/z1188643/dungeonsdata-neurips2022/experiment_code --env WANDB_API_KEY=d2f9309c1cee36dc7ad726c57e4eba04974d9914 --env WANDBPWD=$PWD -B /shared/results/z1188643/dungeons/nle:/nle -B $TMPDIR:/tmp /shared/results/z1188643/dungeons/dungeons.sif 
--- experiment_code/train.sh
+++ experiment_code/train.sh
@@ -4,5 +4,8 @@ export BROKER_IP=0.0.0.0
 export BROKER_PORT=4431
 
 python -m moolib.broker &
+
 # python -m hackrl.experiment connect=$BROKER_IP:$BROKER_PORT exp_point=@-AA-BC num_actor_cpus=20 total_steps=2_000_000_000 actor_batch_size=256 batch_size=128 ttyrec_batch_size=512 supervised_loss=1 adam_learning_rate=0.001 behavioural_clone=True 
 python -m hackrl.experiment connect=$BROKER_IP:$BROKER_PORT exp_point=monk-AA-BC num_actor_cpus=20 total_steps=2_000_000_000 actor_batch_size=256 batch_size=128 ttyrec_batch_size=512 supervised_loss=1 adam_learning_rate=0.001 behavioural_clone=True character='mon-hum-neu-mal'
+
+# python -m debugpy --wait-for-client --listen 5678 ./hackrl/experiment.py connect=$BROKER_IP:$BROKER_PORT exp_point=@-AA-BC num_actor_cpus=20 total_steps=2_000_000_000 actor_batch_size=256 batch_size=128 ttyrec_batch_size=512 supervised_loss=1 adam_learning_rate=0.001 behavioural_clone=True character='mon-hum-neu-mal'
