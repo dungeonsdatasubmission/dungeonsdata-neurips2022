a03a2ab44dc1a4f41620df25ed5c6038ce06124a
Bartłomiej Cupiał <bartomiej.cupial@student.uj.edu.pl>
2023-04-09 18:36:06 +0200

    update runs

--- experiment_code/run_gmum.sh
+++ experiment_code/run_gmum.sh
@@ -4,10 +4,10 @@
 #SBATCH --output=logger-%j.txt
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=10
-#SBATCH --mem-per-cpu=2G
+#SBATCH --cpus-per-task=20
+#SBATCH --mem=20G
 #SBATCH --gres=gpu:1
-#SBATCH --qos=quick  # test (1 GPU, 1 hour), quick (1 GPU, 1 day), normal (2 GPU, 2 days), big (4 GPU, 7 days)
+#SBATCH --qos=big  # test (1 GPU, 1 hour), quick (1 GPU, 1 day), normal (2 GPU, 2 days), big (4 GPU, 7 days)
 #SBATCH --partition=dgxmatinf  # dgxa100 (mini-servers), dgxteam (dgx1 for Team-Net), dgxmatinf (dgx2 for WMiI), dgx (dgx1 and dgx2)
 
 #squeue -u ${USER} --Format "JobID:.6 ,Partition:.4 ,Name:.10 ,StateCompact:.2 ,TimeUsed:.11 ,Qos:.7 ,TimeLeft:.11 ,ReasonList:.16 ,Command:.40"
--- experiment_code/train.sh
+++ experiment_code/train.sh
@@ -5,7 +5,19 @@ export BROKER_PORT=4431
 
 python -m moolib.broker &
 
-# python -m hackrl.experiment connect=$BROKER_IP:$BROKER_PORT exp_point=@-AA-BC num_actor_cpus=20 total_steps=2_000_000_000 actor_batch_size=256 batch_size=128 ttyrec_batch_size=512 supervised_loss=1 adam_learning_rate=0.001 behavioural_clone=True 
-python -m hackrl.experiment connect=$BROKER_IP:$BROKER_PORT exp_point=monk-AA-BC num_actor_cpus=20 total_steps=2_000_000_000 actor_batch_size=256 batch_size=128 ttyrec_batch_size=512 supervised_loss=1 adam_learning_rate=0.001 behavioural_clone=True character='mon-hum-neu-mal'
+# python -m debugpy --wait-for-client --listen 5678 ./hackrl/experiment.py connect=$BROKER_IP:$BROKER_PORT exp_set=2G num_actor_cpus=20 exp_point=monk-APPO  total_steps=2_000_000_000 character='mon-hum-neu-mal'
 
-# python -m debugpy --wait-for-client --listen 5678 ./hackrl/experiment.py connect=$BROKER_IP:$BROKER_PORT exp_point=@-AA-BC num_actor_cpus=20 total_steps=2_000_000_000 actor_batch_size=256 batch_size=128 ttyrec_batch_size=512 supervised_loss=1 adam_learning_rate=0.001 behavioural_clone=True character='mon-hum-neu-mal'
+
+# APPO Experiments
+# python -m hackrl.experiment connect=$BROKER_IP:$BROKER_PORT exp_set=2G num_actor_cpus=20 exp_point=@-APPO     total_steps=2_000_000_000
+# python -m hackrl.experiment connect=$BROKER_IP:$BROKER_PORT exp_set=2G num_actor_cpus=20 exp_point=monk-APPO  total_steps=2_000_000_000 character='mon-hum-neu-mal' group='monk-APPO'
+
+# Behavioural Cloning Experiments
+# python -m hackrl.experiment connect=$BROKER_IP:$BROKER_PORT exp_point=@-AA-BC num_actor_cpus=20 total_steps=2_000_000_000 actor_batch_size=256 batch_size=128 ttyrec_batch_size=512 supervised_loss=1 adam_learning_rate=0.001 behavioural_clone=True group='@-AA-BC'
+# python -m hackrl.experiment connect=$BROKER_IP:$BROKER_PORT exp_point=monk-AA-BC num_actor_cpus=20 total_steps=2_000_000_000 actor_batch_size=256 batch_size=128 ttyrec_batch_size=512 supervised_loss=1 adam_learning_rate=0.001 behavioural_clone=True character='mon-hum-neu-mal' group='monk-AA-BC'
+
+# APPO + Behavioural Cloning Experiments
+# python -m hackrl.experiment connect=$BROKER_IP:$BROKER_PORT exp_point=monk-APPO-AA-BC num_actor_cpus=20 total_steps=2_000_000_000 batch_size=128 ttyrec_batch_size=256 supervised_loss=0.1 exp_set=2G character='mon-hum-neu-mal' group='monk-APPO-AA-BC'
+
+# APPO +  Kickstarting Experiments
+# python -m hackrl.experiment connect=$BROKER_IP:$BROKER_PORT exp_set=2G exp_point=monk-APPO-AA-KS total_steps=2_000_000_000 num_actor_cpus=20 kickstarting_loss=0.1 use_kickstarting=true kickstarting_path=experiment_code/checkpoint/hackrl/nle/group2/checkpoint_v87364.tar  character='mon-hum-neu-mal' group='monk-APPO-AA-KS'
