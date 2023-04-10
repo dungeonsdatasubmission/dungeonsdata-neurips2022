b5ea7a75bd592f6df63b313b342bf49ef95f5cde
Bartłomiej Cupiał <bartomiej.cupial@student.uj.edu.pl>
2023-04-10 15:17:16 +0200

    add path to kickstarting checkpoint

--- experiment_code/train.sh
+++ experiment_code/train.sh
@@ -9,7 +9,7 @@ python -m moolib.broker &
 
 
 # APPO Experiments
-# python -m hackrl.experiment connect=$BROKER_IP:$BROKER_PORT exp_set=2G num_actor_cpus=20 exp_point=@-APPO     total_steps=2_000_000_000
+# python -m hackrl.experiment connect=$BROKER_IP:$BROKER_PORT exp_set=2G num_actor_cpus=20 exp_point=@-APPO     total_steps=2_000_000_000 group='@-APPO'
 # python -m hackrl.experiment connect=$BROKER_IP:$BROKER_PORT exp_set=2G num_actor_cpus=20 exp_point=monk-APPO  total_steps=2_000_000_000 character='mon-hum-neu-mal' group='monk-APPO'
 
 # Behavioural Cloning Experiments
@@ -20,4 +20,4 @@ python -m moolib.broker &
 # python -m hackrl.experiment connect=$BROKER_IP:$BROKER_PORT exp_point=monk-APPO-AA-BC num_actor_cpus=20 total_steps=2_000_000_000 batch_size=128 ttyrec_batch_size=256 supervised_loss=0.1 exp_set=2G character='mon-hum-neu-mal' group='monk-APPO-AA-BC'
 
 # APPO +  Kickstarting Experiments
-# python -m hackrl.experiment connect=$BROKER_IP:$BROKER_PORT exp_set=2G exp_point=monk-APPO-AA-KS total_steps=2_000_000_000 num_actor_cpus=20 kickstarting_loss=0.1 use_kickstarting=true kickstarting_path=experiment_code/checkpoint/hackrl/nle/group2/checkpoint_v87364.tar  character='mon-hum-neu-mal' group='monk-APPO-AA-KS'
+# python -m hackrl.experiment connect=$BROKER_IP:$BROKER_PORT exp_set=2G exp_point=monk-APPO-AA-KS total_steps=2_000_000_000 num_actor_cpus=20 kickstarting_loss=0.1 use_kickstarting=true kickstarting_path=/net/pr2/projects/plgrid/plgggmum_crl/bcupial/monk-AA-BC/checkpoint_v60242.tar  character='mon-hum-neu-mal' group='monk-APPO-AA-KS'
