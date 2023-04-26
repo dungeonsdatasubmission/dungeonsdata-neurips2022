1e6645b6d2b91943294ab9c2226dc4508bcedd59
BartekCupial <92169405+BartekCupial@users.noreply.github.com>
2023-04-26 18:42:40 +0200

    single node multigpu (#12)

--- experiment_code/mrunner_run.py
+++ experiment_code/mrunner_run.py
@@ -1,5 +1,7 @@
 import subprocess
 
+import torch
+
 from mrunner.helpers.client_helper import get_configuration
 
 
@@ -14,6 +16,15 @@ if __name__ == "__main__":
     key_pairs = [f"{key}={value}" for key, value in cfg.items()]
     cmd = ['python', '-m', 'hackrl.experiment'] + key_pairs
 
+    device_count = torch.cuda.device_count()
+    if device_count > 1:
+        if "device" not in cfg or cfg["device"] != "cpu":
+            # Adding more peers to this experiment, starting more processes with the
+            # same `project` and `group` settings, using a different setting for `device`           
+            for i in range(1, device_count):
+                subprocess.Popen(cmd + [f"device=cuda:{i}"])
+
+    # default device is cuda:0
     subprocess.run(cmd) 
 
     # When you're done, terminate the broker process
